de53c62722b3191ef0cc8cb371dd441c9d0de04e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-08-27 00:31:52 +0200

    Better frontend

--- public/map.php
+++ public/map.php
@@ -9,7 +9,9 @@
     <title>Mapa Popiš památku!</title>
   </head>
   <body>
-    <div id="map"></div>
+		<div id="map"></div>
+		<label for="startswith">Najít pouze kulturní památky začínající na: </label><input type="text" id="startswith">
+		<button type="button" onclick="send()">Odeslat</button>
 
     <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
     <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/leaflet/1.3.1/leaflet.js"></script>
@@ -20,27 +22,31 @@
 		?>
 		<script src="get_monuments.py?startswith=<?php echo $startswith ?>"></script>
     <script>
+			function send()
+			{
+				window.location.html = "https://tools.wmflabs.org/map-of-monuments/map.php?startswith=" + document.getElementById('startswith').value;
+			}
     	var style = 'osm-intl';
-	var server = 'https://maps.wikimedia.org/';
+			var server = 'https://maps.wikimedia.org/';
 
-	// Create a map
-	var map = L.map('map');
-	map.setView([50, 16], 7);
-	L.tileLayer(server + style + '/{z}/{x}/{y}.png', {
-		    maxZoom: 18,
-		    id: 'wikipedia-map-01',
-		    attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-	}).addTo(map);
-	
-	var markers = L.markerClusterGroup();
-	for (var i = 0; i < addressPoints.length; i++) {
-		var a = addressPoints[i];
-		var title = a[2];
-		var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
-		marker.bindPopup(title);
-		markers.addLayer(marker);
-	}
-	map.addLayer(markers);
+			// Create a map
+			var map = L.map('map');
+			map.setView([50, 16], 7);
+			L.tileLayer(server + style + '/{z}/{x}/{y}.png', {
+						maxZoom: 18,
+						id: 'wikipedia-map-01',
+						attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+			}).addTo(map);
+			
+			var markers = L.markerClusterGroup();
+			for (var i = 0; i < addressPoints.length; i++) {
+				var a = addressPoints[i];
+				var title = a[2];
+				var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
+				marker.bindPopup(title);
+				markers.addLayer(marker);
+			}
+			map.addLayer(markers);
     </script>
   </body>
 </html>
